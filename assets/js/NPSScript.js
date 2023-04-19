f97522d0dbafbaa8bc9a03f29db72ca492ee1980
Romaney-M <rmugoodwin001@gmail.com>
2023-04-19 17:53:47 -0500

    hiking

--- assets/js/NPSScript.js
+++ assets/js/NPSScript.js
@@ -1,7 +1,28 @@
-async function logJSONData() {
-    const response = await fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad');
-    const jsonData = await response.json();
-    console.log(jsonData);
-}
+// async function logJSONData() {
+//     const response = await fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad');
+//     const jsonData = await response.json();
+//     console.log(jsonData);
+// }
 
-logJSONData();
+// logJSONData();
+    var APIKey = "MNJumTqMuNY0igarIiSXmJBHSmFPcL0FajFPRD0a";
+    var Hiking;
+    var queryURL = "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking" + "&api_key=" + APIKey;
+
+fetch(queryURL)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+    })    
+
+//     var sQuearyURL = "https://developer.nps.gov/api/v1/places?q=" + inputOption + "&api_key=" + APIKey;
+
+// fetch(sQuearyURL)
+// .then(res => {
+//     return res.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })    
