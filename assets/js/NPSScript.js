3a10a3f78faa60ec4d7a25b14d292a9274843f7c
KalebAguirre <126530242+KalebAguirre@users.noreply.github.com>
2023-04-24 18:56:17 -0500

    Revert "the state list"

--- assets/js/NPSScript.js
+++ assets/js/NPSScript.js
@@ -5,57 +5,24 @@
 // }
 
 // logJSONData();
-var changeState = document.querySelector('.stateInput');
+    var APIKey = "MNJumTqMuNY0igarIiSXmJBHSmFPcL0FajFPRD0a";
+    var Hiking;
+    var queryURL = "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking" + "&api_key=" + APIKey;
 
-let newState;
-
-var APIKey = "MNJumTqMuNY0igarIiSXmJBHSmFPcL0FajFPRD0a";
-var Hiking;
-var queryURL = "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking" + "&api_key=" + APIKey;
-
-
-
-function stateCompare(data, newState){
-    for(var i = 0; i<data.data[0].parks.length;i ++){
-        if(newState === data.data[0].parks[i].states){
-            console.log('match')
-            // redering in here
-        }
-    }
-}
-
-const getMap = (state) => {
-    console.log(state);
-    if(state === "Alabama"){
-        newState = "AL"
-    } else { 
-        if(state === "Alaska"){
-            newState = "AK"
-    } else {
-        if(state === "Arizona"){
-            newState = "AZ"
-        }
-    } else {
-        if(state === "Arkansas"){
-            newState = "AR"
-        }
-    }
-    }
-
-    fetch(queryURL)
-.then(res => {
-    return res.json();
-})
-.then(data => {
-    console.log(data.data[0].parks.states);
-    console.log(data);
-    console.log(newState);
-    stateCompare(data, newState);
-}) 
-}
- 
-
-
-    inputOption.addEventListener("change", function (event) {
-        getMap(`${event.target.value}`)
+fetch(queryURL)
+    .then(res => {
+        return res.json();
     })
+    .then(data => {
+        console.log(data);
+    })    
+
+//     var sQuearyURL = "https://developer.nps.gov/api/v1/places?q=" + inputOption + "&api_key=" + APIKey;
+
+// fetch(sQuearyURL)
+// .then(res => {
+//     return res.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })    
